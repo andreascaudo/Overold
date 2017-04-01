88e6acb1e94bdac29984c2d9954891213a927388
gkpro1 <scaudo.andrea@gmail.com>
2017-04-01 18:08:31 +0200

    Error test

--- Overold/src/main/java/HelloServlet.java
+++ Overold/src/main/java/HelloServlet.java
@@ -19,7 +19,7 @@ public class HelloServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     
-    }
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
