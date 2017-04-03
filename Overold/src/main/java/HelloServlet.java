43f5e64750b2bbb659915dc2a8bb7c4554f53ced
gkpro1 <scaudo.andrea@gmail.com>
2017-04-03 18:42:24 +0200

    Subito integration #1

--- Overold/src/main/java/HelloServlet.java
+++ Overold/src/main/java/HelloServlet.java
@@ -1,30 +1,44 @@
 
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  * Servlet implementation class HelloServlet
  */
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HelloServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private String regioneSelezionata;
+	private PrintWriter out;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		out = response.getWriter();
+		out.print("Welcome! \n");
 	}
 
 	/**
@@ -33,6 +47,43 @@ public class HelloServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		String item= request.getParameter("item");
+		String region= request.getParameter("region");
+		out = response.getWriter();
+		out.print("Do you need ");
+		out.print(item);
+		out.print(" in ");
+		out.print(region);
+		out.print("\n");
+		String search = "http://www.subito.it/annunci-"+ region + "/vendita/usato/?q=" + item;
+		System.out.println(search);
+		search = search.replaceAll(" ","+");
+		out.print(executePost(search));
+	}
+
+	public static String executePost(String targetURL) {
+		URL subito;
+		StringBuffer sbb = new StringBuffer();
+		try {
+			subito = new URL(targetURL);
+
+			URLConnection sb = subito.openConnection();
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(
+							sb.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null){
+				sbb.append(inputLine);
+				sbb.append('\r');
+			}
+			in.close();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return sbb.toString();
 	}
 
 }
--- Overold/src/main/webapp/index.jsp
+++ Overold/src/main/webapp/index.jsp
@@ -1,5 +1,31 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+	<h2>Hello World!</h2>
+	<form method="Post" action="HelloServlet">
+		What do you need?<br>
+		<input type="text" name="item" /><br>
+		<select name="region" size="1">
+	 		<option value="abruzzo">Abruzzo</option>
+	  		<option value="basilicata">Basilicata</option>
+	  		<option value="calabria">Calabria</option>
+	  		<option value="campania">Campania</option>
+	  		<option value="emilia-Romagna">Emilia Romagna</option>
+	  		<option value="friuli-Venezia-Giulia">Friuli-Venezia Giulia</option>
+	  		<option value="lazio">Lazio</option>
+	  		<option value="liguria">Liguria</option>
+	  		<option value="lombardia">Lombardia</option>
+	  		<option value="marche">Marche</option>
+	  		<option value="molise">Molise</option>
+	  		<option value="piemonte">Piemonte</option>
+	  		<option value="puglia">Puglia</option>
+	  		<option value="sardegna">Sardegna</option>
+	  		<option value="sicilia">Sicilia</option>
+	  		<option value="trentino-Alto-Adige">Trentino-Alto Adige</option>
+	  		<option value="umbria">Umbria</option>
+	  		<option value="valle-d-aosta">Valle d'Aosta</option>
+	  		<option value="veneto">Veneto</option>
+		</select>
+		<input type="submit" value="Send" />
+	</form>
 </body>
 </html>
